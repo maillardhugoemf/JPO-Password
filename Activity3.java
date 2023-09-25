5b8718ba6497805abf193ecb66fed0b3492a00d2
SILMEY <48353440+SILMEY@users.noreply.github.com>
2023-09-25 13:50:41 +0200

    Update Activity3.java

--- Activity3.java
+++ Activity3.java
@@ -1,7 +1,7 @@
 import java.util.Base64;
 
 public class Activity3 {
-   public static final String PASSWORD = "YnJhdjA";
+   public static final String PASSWORD = "bTBsZHU=";
     public static final char[] CHARACTERS = new char[]{'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
     public static final int MAXCHAR = 5;
 
